614b871dc0bbe869dbf6243fcd156224e544baad
brenogoncalves <thoughtworks.goncalves@mercedes-benz.ca>
2025-11-23 16:51:44 -0300

    innicial commit

---
+++ src/main/java/com/breno/llm/llm_microservice_observability_demo/LlmMicroserviceObservabilityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.breno.llm.llm_microservice_observability_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmMicroserviceObservabilityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmMicroserviceObservabilityDemoApplication.class, args);
+	}
+
+}
